997842cfe90671afb8ace3798d2bff79df5385a2
alkhimenok <alkhimenok.kirill.leonidovich@gmail.com>
2022-06-12 15:20:03 +0300

    fix: bundle config

--- .babelrc.js
+++ .babelrc.js
@@ -1,12 +1,7 @@
-module.exports = {
-  presets: [
-    [
-      '@babel/preset-env',
-      {
-        targets: {
-          node: 'current'
-        }
-      }
-    ]
-  ]
+module.exports = function (api) {
+  api.cache(true)
+
+  return {
+    presets: ['@babel/preset-env']
+  }
 }
