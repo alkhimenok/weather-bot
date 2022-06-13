c8b139bc3152366e295771894119343db8506afb
alkhimenok <alkhimenok.kirill.leonidovich@gmail.com>
2022-06-13 03:37:48 +0300

    add: handler for language switch

--- src/Handlers.js
+++ src/Handlers.js
@@ -1,14 +1,41 @@
-const { t } = require('./utils/lang')
+const { langList, t, switchLang } = require('./utils/lang')
+const availableLangs = require('./constants/availableLangs')
 
 class Handlers {
   constructor(bot) {
     this.bot = bot
 
     this.handleStart = this.handleStart.bind(this)
+    this.handleSwitchLang = this.handleSwitchLang.bind(this)
   }
 
-  handleStart(message, messageTypes) {
-    this.bot.sendMessage(message.chat.id, t('welcome')).then(() => console.log('send'))
+  async handleStart({ chat }) {
+    const { id, first_name, last_name, username } = chat
+    const keyboard = [langList.map((lang) => ({ text: availableLangs[lang] }))]
+    const option = this._getDefaultKeyboardOptions(keyboard)
+
+    await this.bot.sendMessage(id, t('fillProfile.lang.select'), option)
+  }
+
+  async handleSwitchLang({ chat, text }) {
+    const { entries, fromEntries } = Object
+    const availableLangsEntries = entries(availableLangs).map(([key, value]) => [value, key])
+    const selectedLang = fromEntries(availableLangsEntries)[text]
+
+    switchLang(selectedLang)
+
+    const { id } = chat
+    const lang = availableLangs[selectedLang]
+    const keyboard = [[{ text: t('actions.continue') }, { text: t('actions.change') }]]
+    const option = this._getDefaultKeyboardOptions(keyboard)
+
+    await this.bot.sendMessage(id, t('fillProfile.lang.confirm', { lang }), option)
+  }
+
+  _getDefaultKeyboardOptions(keyboard) {
+    const data = { keyboard, resize_keyboard: true, one_time_keyboard: true }
+
+    return { reply_markup: JSON.stringify(data) }
   }
 }
 
