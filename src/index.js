bc64189f865fb06add3565152e421740c39215c1
alkhimenok <alkhimenok.kirill.leonidovich@gmail.com>
2022-06-13 03:40:00 +0300

    fix: root bot

--- src/index.js
+++ src/index.js
@@ -2,10 +2,10 @@ require('dotenv').config({ path: '.env' })
 
 const TelegramBot = require('node-telegram-bot-api')
 
-const initLang = require('./utils/lang')
 const Handlers = require('./Handlers')
 const Events = require('./Events')
 const Commands = require('./Commands')
+const { initLang } = require('./utils/lang')
 
 const { BOT_TOKEN: token } = process.env
 const bot = new TelegramBot(token, { polling: true })
