5f9a5da4b651fc15ccca0dbf577054c94b4804cb
alkhimenok <alkhimenok.kirill.leonidovich@gmail.com>
2022-06-13 16:03:05 +0300

    add: handlers for change language

--- src/Handlers.js
+++ src/Handlers.js
@@ -2,40 +2,85 @@ const { langList, t, switchLang } = require('./utils/lang')
 const availableLangs = require('./constants/availableLangs')
 
 class Handlers {
-  constructor(bot) {
+  constructor(bot, user) {
     this.bot = bot
+    this.user = user
 
     this.handleStart = this.handleStart.bind(this)
     this.handleSwitchLang = this.handleSwitchLang.bind(this)
+    this.handleConfirmLang = this.handleConfirmLang.bind(this)
+    this.handleChangeLang = this.handleChangeLang.bind(this)
   }
 
   async handleStart({ chat }) {
     const { id, first_name, last_name, username } = chat
-    const keyboard = [langList.map((lang) => ({ text: availableLangs[lang] }))]
-    const option = this._getDefaultKeyboardOptions(keyboard)
+    const title = t('fillProfile.lang.select')
+    const keyboard = this._getLandKeyboard()
+    const option = this._getDefaultInlineKeyboardOptions(keyboard)
 
-    await this.bot.sendMessage(id, t('fillProfile.lang.select'), option)
+    await this._sendMessage(id, title, option)
   }
 
-  async handleSwitchLang({ chat, text }) {
-    const { entries, fromEntries } = Object
-    const availableLangsEntries = entries(availableLangs).map(([key, value]) => [value, key])
-    const selectedLang = fromEntries(availableLangsEntries)[text]
-
+  async handleSwitchLang({ message }, selectedLang) {
     switchLang(selectedLang)
 
-    const { id } = chat
     const lang = availableLangs[selectedLang]
-    const keyboard = [[{ text: t('actions.continue') }, { text: t('actions.change') }]]
-    const option = this._getDefaultKeyboardOptions(keyboard)
+    const title = t('fillProfile.lang.confirm', { lang })
+    const keyboard = this._getLangSwitchFinishKeyboard()
+    const option = this._getEditDefaultInlineKeyboardOptions(keyboard, message)
 
-    await this.bot.sendMessage(id, t('fillProfile.lang.confirm', { lang }), option)
+    await this._editMessage(title, option)
+  }
+
+  async handleConfirmLang(query) {
+    console.log('handleConfirmLang')
+  }
+
+  async handleChangeLang({ message }) {
+    const title = t('fillProfile.lang.change')
+    const keyboard = this._getLandKeyboard()
+    const option = this._getEditDefaultInlineKeyboardOptions(keyboard, message)
+
+    await this._editMessage(title, option)
+  }
+
+  async _sendMessage(id, title, options) {
+    await this.bot.sendMessage(id, title, options)
+  }
+
+  async _editMessage(title, options) {
+    await this.bot.editMessageText(title, options)
+  }
+
+  _getDefaultInlineKeyboardOptions(inline_keyboard) {
+    const data = { inline_keyboard }
+
+    return { reply_markup: JSON.stringify(data) }
+  }
+
+  _getEditDefaultInlineKeyboardOptions(inline_keyboard, { message_id, chat }) {
+    const data = { inline_keyboard }
+    const { id: chat_id } = chat
+
+    return { reply_markup: JSON.stringify(data), message_id, chat_id }
   }
 
   _getDefaultKeyboardOptions(keyboard) {
     const data = { keyboard, resize_keyboard: true, one_time_keyboard: true }
 
     return { reply_markup: JSON.stringify(data) }
+  }
+
+  _getLandKeyboard() {
+    const callback = (lang) => ({ text: availableLangs[lang], callback_data: `lang:${lang}` })
+
+    return [langList.map(callback)]
+  }
+
+  _getLangSwitchFinishKeyboard() {
+    const callback = (action) => ({ text: t(`actions.${action}`), callback_data: `${action}Lang` })
+
+    return [['confirm', 'change'].map(callback)]
   }
 }
 
