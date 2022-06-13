09783a31d6c0fe73803e8423f1336fb141308a78
alkhimenok <alkhimenok.kirill.leonidovich@gmail.com>
2022-06-13 16:04:21 +0300

    refactor: root bot

--- src/index.js
+++ src/index.js
@@ -2,16 +2,17 @@ require('dotenv').config({ path: '.env' })
 
 const TelegramBot = require('node-telegram-bot-api')
 
+const User = require('./User')
 const Handlers = require('./Handlers')
 const Events = require('./Events')
 const Commands = require('./Commands')
 const { initLang } = require('./utils/lang')
 
-const { BOT_TOKEN: token } = process.env
-const bot = new TelegramBot(token, { polling: true })
-
 const runBot = () => {
-  const handlers = new Handlers(bot)
+  const { BOT_TOKEN: token } = process.env
+  const bot = new TelegramBot(token, { polling: true })
+  const user = new User()
+  const handlers = new Handlers(bot, user)
   const { initEvents } = new Events(bot, handlers)
   const { initCommands } = new Commands(bot)
 
