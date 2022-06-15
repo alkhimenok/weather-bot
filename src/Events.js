61f07b28f2a82831ed2213c0723bfebd693e94cc
alkhimenok <alkhimenok.kirill.leonidovich@gmail.com>
2022-06-15 23:23:00 +0300

    add: events

--- src/Events.js
+++ src/Events.js
@@ -2,48 +2,58 @@ const commandTypes = require('./constants/commandTypes')
 const queryTypes = require('./constants/queryTypes')
 
 class Events {
-  constructor(bot, { handleStart, handleSetLang, handleConfirmLang, handleChangeLang }) {
+  constructor(bot, handlers) {
     this.bot = bot
-    this.handleStart = handleStart
-    this.handleSetLang = handleSetLang
-    this.handleConfirmLang = handleConfirmLang
-    this.handleChangeLang = handleChangeLang
+    this.handleStart = handlers.handleStart
+    this.handleSetLang = handlers.handleSetLang
+    this.handleConfirmLang = handlers.handleConfirmLang
+    this.handleChangeLang = handlers.handleChangeLang
+
+    this.handleMessageCatch = handlers.handleMessageCatch
+    this.handleQueryCatch = handlers.handleQueryCatch
 
     this.initEvents = this.initEvents.bind(this)
   }
 
   initEvents() {
     this.bot.on('message', this._initMessageEvents.bind(this))
-    this.bot.on('callback_query', this._initCallbackEvents.bind(this))
+    this.bot.on('callback_query', this._initQueryEvents.bind(this))
+    this.bot.on('polling_error', this._initPollingErrorEvents.bind(this))
   }
 
   _initMessageEvents(message, metadata) {
     const type = message.text
 
-    try {
-      const handler = {
-        [commandTypes.start]: this.handleStart
-      }[type]
+    const handler = {
+      [commandTypes.start]: this.handleStart
+    }[type]
 
-      handler(message, metadata)
-    } catch (e) {
-      console.warn('_initMessageEvents', e)
-    }
+    this._abstractEvent(handler, this.handleMessageCatch, type, message, metadata)
   }
 
-  _initCallbackEvents(query) {
+  _initQueryEvents(query) {
     const [type, option] = query.data.split(':')
 
-    try {
-      const handler = {
-        [queryTypes.setLang]: this.handleSetLang,
-        [queryTypes.confirmLang]: this.handleConfirmLang,
-        [queryTypes.changeLang]: this.handleChangeLang
-      }[type]
+    const handler = {
+      [queryTypes.setLang]: this.handleSetLang,
+      [queryTypes.confirmLang]: this.handleConfirmLang,
+      [queryTypes.changeLang]: this.handleChangeLang
+    }[type] // ?? handleSearchRegion
 
-      handler(query, option)
+    this._abstractEvent(handler, this.handleQueryCatch, type, query, option)
+  }
+
+  _initPollingErrorEvents(...error) {
+    console.log('error', error)
+  }
+
+  _abstractEvent(handler, handleCatch, type, ...params) {
+    console.log('handler', handler.name)
+
+    try {
+      handler(...params)
     } catch (e) {
-      console.warn('_initCallbackEvents', e)
+      handleCatch(type, e)
     }
   }
 }
