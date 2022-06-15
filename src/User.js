436ea2aa3e974349000730a1cdb8bd1423bb0fc5
alkhimenok <alkhimenok.kirill.leonidovich@gmail.com>
2022-06-15 23:23:32 +0300

    add: user data

--- src/User.js
+++ src/User.js
@@ -1,5 +1,50 @@
+const { uid } = require('uid')
+const userConnectionStatuses = require('./constants/userConnectionStatuses')
+
 class User {
-  constructor() {}
+  constructor() {
+    this.updateData = this.updateData.bind(this)
+  }
+
+  data = {
+    id: uid(),
+    chatId: null,
+    firstName: null,
+    lastName: null,
+    username: null,
+    status: userConnectionStatuses.missing,
+    lang: null,
+    location: null,
+    mailing: null
+  }
+
+  get chatId() {
+    return this.data.chatId
+  }
+
+  get firstName() {
+    return this.data.firstName
+  }
+
+  get lastName() {
+    return this.data.lastName
+  }
+
+  get userName() {
+    return this.data.username
+  }
+
+  get status() {
+    return this.data.status
+  }
+
+  get isConnected() {
+    return this.status === userConnectionStatuses.connected
+  }
+
+  updateData(data) {
+    this.data = { ...this.data, ...data }
+  }
 }
 
 module.exports = User
